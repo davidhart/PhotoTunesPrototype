110709c432f4012df42b2ed7fc29c67b8cf53227
David Hart <david.hart555@googlemail.com>
2012-01-23 14:06:14 +0000

    Added unit test for image slice

--- ImageProcessing/ImageProcessingTests/ImageProcessingTests.m
+++ ImageProcessing/ImageProcessingTests/ImageProcessingTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "ImageProcessingTests.h"
+#import "ImageSlice.h"
 
 @implementation ImageProcessingTests
 
@@ -24,9 +25,36 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testImageSlice
 {
-    STFail(@"Unit tests are not implemented yet in ImageProcessingTests");
+    ImageSlice* slice = [ImageSlice alloc];
+    STAssertNotNil(slice, @"Error creating slice");
+    
+    UInt8 pixels [] = { 0xFFu, 0xFFu, 0xFFu, 0xFFu };
+    [slice init: pixels: 4: 1];
+    
+    STAssertEquals([slice getAverageRed], pixels[0], @"Error average red value incorrect (1x1 image)");
+    STAssertEquals([slice getAverageGreen], pixels[1], @"Error average green value incorrect (1x1 image)");
+    STAssertEquals([slice getAverageBlue], pixels[2], @"Error average blue value incorrect (1x1 image)");
+    
+    UInt8 pixels2 [] ={ 0xFFu, 0xFFu, 0xFFu, 0xFFu, 
+        0x0u, 0x0u, 0x0u, 0xFFu };
+    
+    [slice init: pixels2: 4: 2];
+    
+    STAssertEquals([slice getAverageRed], (UInt8)127, @"Error average red value incorrect (1x2 image)");
+    STAssertEquals([slice getAverageGreen], (UInt8)127, @"Error average green value incorrect (1x2 image)");
+    STAssertEquals([slice getAverageBlue], (UInt8)127, @"Error average blue value incorrect (1x2 image)");
+    
+    
+    UInt8 pixels3 [] ={ 0x2u, 0x2u, 0x2u, 0x2u, 0x20u, 0x20u, 0x20u, 0xFFu, 
+        0x0u, 0x0u, 0x0u, 0xFFu, 0x0u, 0x0u, 0x0u, 0xFFu };
+    
+    [slice init: pixels3: 8: 2];
+    
+    STAssertEquals([slice getAverageRed], (UInt8)0x1u, @"Error average red value incorrect (2x2 image)");
+    STAssertEquals([slice getAverageGreen], (UInt8)0x1u, @"Error average green value incorrect (2x2 image)");
+    STAssertEquals([slice getAverageBlue], (UInt8)0x1u, @"Error average blue value incorrect (2x2 image)");
 }
 
 @end
