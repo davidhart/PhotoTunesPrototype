e0a6e8358dba1bc2d28844d81f4ee59c841e11e7
David Hart <david.hart555@googlemail.com>
2012-01-23 15:10:53 +0000

    Added ImagePropreties Unit Test

--- ImageProcessing/ImageProcessingTests/ImageProcessingTests.m
+++ ImageProcessing/ImageProcessingTests/ImageProcessingTests.m
@@ -8,6 +8,7 @@
 
 #import "ImageProcessingTests.h"
 #import "ImageSlice.h"
+#import "ImageProperties.h"
 
 @implementation ImageProcessingTests
 
@@ -55,6 +56,71 @@
     STAssertEquals([slice getAverageRed], (UInt8)0x1u, @"Error average red value incorrect (2x2 image)");
     STAssertEquals([slice getAverageGreen], (UInt8)0x1u, @"Error average green value incorrect (2x2 image)");
     STAssertEquals([slice getAverageBlue], (UInt8)0x1u, @"Error average blue value incorrect (2x2 image)");
+}
+
+- (void)testImageProperties
+{
+    UInt8 pixels [5*5*4];
+    
+    int c = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+        pixels[c++] = 0xFF;
+        pixels[c++] = 0xFF;
+        pixels[c++] = 0xFF;
+        pixels[c++] = 0xFF;
+        
+        pixels[c++] = 0x00;
+        pixels[c++] = 0x00;
+        pixels[c++] = 0x00;
+        pixels[c++] = 0xFF;
+        
+        pixels[c++] = 0xFF;
+        pixels[c++] = 0x00;
+        pixels[c++] = 0x00;
+        pixels[c++] = 0xFF;
+        
+        pixels[c++] = 0x00;
+        pixels[c++] = 0xFF;
+        pixels[c++] = 0x00;
+        pixels[c++] = 0xFF;
+        
+        pixels[c++] = 0x00;
+        pixels[c++] = 0x00;
+        pixels[c++] = 0xFF;
+        pixels[c++] = 0xFF;
+    };
+    
+    
+    CGContextRef context;
+	context = CGBitmapContextCreate(pixels,
+                                    5,
+                                    5,
+                                    8,
+                                    5*4,
+                                    CGColorSpaceCreateDeviceRGB(),
+                                    kCGImageAlphaNoneSkipLast);
+    
+	CGImageRef newCGImage = CGBitmapContextCreateImage(context);
+	UIImage *image = [UIImage imageWithCGImage:newCGImage];
+	CGContextRelease(context);
+    
+    ImageProperties* imageProperties = [ImageProperties alloc];
+    STAssertNotNil(imageProperties, @"Error could not create image properties");
+    
+    [imageProperties init:image];
+    
+    STAssertEquals([imageProperties numSlices], 5u, @"Error incorrect number of slices");
+    
+    for (int i = 0; i < 5; ++i)
+    {
+        ImageSlice* slice = [imageProperties getSlice:i];
+        STAssertNotNil(slice, @"Error getSlice returned null");
+        
+        STAssertEquals([slice getAverageRed], pixels[i*4], @"Error incorrect average red");
+        STAssertEquals([slice getAverageGreen], pixels[i*4+1], @"Error incorrect average green");
+        STAssertEquals([slice getAverageBlue], pixels[i*4+2], @"Error incorrect average blue");
+    }
 }
 
 @end
